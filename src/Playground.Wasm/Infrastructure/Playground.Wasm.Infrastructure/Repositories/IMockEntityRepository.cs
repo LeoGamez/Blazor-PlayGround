1f1581be8c57829b019292b98ddaeab17c777778
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2023-08-16 17:57:09 -0600

    added mocked repository and entity

---
+++ src/Playground.Wasm/Infrastructure/Playground.Wasm.Infrastructure/Repositories/IMockEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿using Playground.Wasm.Domain.Entities;
+
+namespace Playground.Wasm.Infrastructure.Repositories
+{
+    public interface IMockEntityRepository
+    {
+        Task<IEnumerable<MockEntity>> ReadAll();
+    }
+}
