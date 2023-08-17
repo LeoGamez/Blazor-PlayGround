0374801c6f4a07be50d5e62c1ee2c7dafa04cc1e
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2023-08-17 11:35:11 -0600

    added breadcrumbs

---
+++ src/Playground.Wasm/Shared/Helpers/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.Wasm.Shared.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharSubstring(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+            return $"{input[0].ToString().ToUpper()}{input.Substring(1)}";
+        }
+    }
+}
