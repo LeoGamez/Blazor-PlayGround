fcf3ad73d3f1b741f4d5e486135eccd3c958b173
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2023-08-21 16:30:20 -0600

    Added Leaflet example

--- src/Playground.Wasm/Client/Program.cs
+++ src/Playground.Wasm/Client/Program.cs
@@ -1,3 +1,4 @@
+using FisSst.BlazorMaps.DependencyInjection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -15,6 +16,9 @@ namespace Playground.Wasm.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
+
+            builder.Services.AddBlazorLeafletMaps();
+
             await builder.Build().RunAsync();
         }
     }
